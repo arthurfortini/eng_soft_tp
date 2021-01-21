3e6c318f7ce633490a925acf134bfa12220a8ef4
Augusto <vilaca.aug@gmail.com>
2021-01-20 21:50:57 -0300

    html e css de telas do login e create account criadas. Adicao do botao para redirecionar a tela de criacao de conta

---
+++ gambit/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AccountComponent } from './tela/account/account.component';
+import { LoginComponent } from './tela/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccountComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
